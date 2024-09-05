bb373935283ac6d99e777b5d7450efac3d137fdb
lorhee <hklr.son@gmail.com>
2024-09-05 14:17:35 -0400

    renamed variables

--- corn.py
+++ corn.py
@@ -1,26 +1,25 @@
 ﻿import math
 
-pricedozencorn = 10
-pricehalfdozen=5.5
-priceonecorn = 1
-countofcorn = int(input ("Please enter quantity of corn: "))
+price_dozen_corn = 10
+price_half_dozen = 5.5
+price_one_corn = 1
+corn_count = int(input ("Please enter quantity of corn: "))
 
-dozencorn=0
-halfdozencorn=0
-singlecorn=0
-if (countofcorn >=12):
-    dozencorn=math.floor(countofcorn/12)
-    print (dozencorn)
+dozen_corn = 0
+half_dozen_corn = 0
+single_corn = 0
+if (corn_count >= 12):
+    dozen_corn = math.floor(corn_count/12)
+    print (dozen_corn)
 
-if ((countofcorn%12)!=0):
-    halfdozencorn= math.floor((countofcorn-dozencorn*12)/6)
-    print(halfdozencorn)
+if ((corn_count % 12) != 0):
+    half_dozen_corn = math.floor((corn_count - dozen_corn*12)/6)
+    print(half_dozen_corn)
 #elif((countofcorn%6)!=0):
 #else:
-    singlecorn=(countofcorn-(dozencorn*12)-(halfdozencorn*6))
-    print(singlecorn)
+    single_corn = (corn_count - (dozen_corn*12) - (half_dozen_corn*6))
+    print(single_corn)
 
-
-costofcorn =dozencorn*pricedozencorn+halfdozencorn*pricehalfdozen+singlecorn*priceonecorn
-print ("Quantity of corn purchased =", countofcorn)
-print ("Cost of corn = $", costofcorn)
+corn_cost = dozen_corn * price_dozen_corn + half_dozen_corn * price_half_dozen + single_corn * price_one_corn
+print ("Quantity of corn purchased =", corn_count)
+print ("Cost of corn = $", corn_cost)
